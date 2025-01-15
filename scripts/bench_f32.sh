74f2e28ec7f1dc8d365f765dafb61d8d415f2419
olivergraininge <oliver@grainge.me>
2025-01-15 13:38:44 +0000

    adding benchmark and chat scripts for openelm model

--- scripts/bench_f32.sh
+++ scripts/bench_f32.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-llama-bench -m models/gguf_models/OpenELM-3B-Instruct-f32.gguf 
+llama-bench -m models/gguf_models/OpenELM-3B-Instruct-f32.gguf -p 128 -n 1 --batch-size 128 --threads 1
--- scripts/chat_f16.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-llama-cli -m models/gguf_models/OpenELM-3B-Instruct-f16.gguf -p "Could you write a very simpleprogram in c++ to print hello world in less than 10 lines of code?"
